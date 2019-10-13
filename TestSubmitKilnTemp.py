cbf4cc03c0d5f87c40c324a22fdcefb1aba56298
Brian Muth <bmuth@telus.net>
2019-10-13 12:20:21 -0700

    test socket transfer of data

--- TestSubmitKilnTemp.py
+++ TestSubmitKilnTemp.py
@@ -1,12 +1,29 @@
 import socket
+import os
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
-host = socket.gethostbyname(socket.gethostname())
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((host, PORT))
-    s.sendall(b'X')
-    data = s.recv(1024)
-print('Response: [{0}]'.format (data.decode('utf-8')))
+def SubmitFile (filename):
+    host = socket.gethostbyname(socket.gethostname())
+
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, PORT))
+            with open(filename, "rb") as f:
+                byte = f.read(4096)
+                while byte != b"":
+                    sa.send (byte)
+                    byte = f.read(4096)
+                data = s.recv(1024)
+                print('Response: [{0}]'.format (data.decode('utf-8')))
+    except ConnectionError as e:
+        print ("connection error for host {0} {1} {2}".format(HOST, e.errno, e.strerror))
+
+    except IOError as e:
+        print ("transmission error on file {0} {1} {2}".format(filename, e.errno, e.strerror))
+
+filename = "./data/image/xxx.png"
+SubmitFile (filename)
+

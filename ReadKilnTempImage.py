3db0764cc0b6164263535175da26c0a35f58585d
Brian Muth <bmuth@telus.net>
2019-10-17 07:45:20 -0700

    Can transfer file

--- ReadKilnTempImage.py
+++ ReadKilnTempImage.py
@@ -1,33 +1,58 @@
 import socket
+import struct
+import os
 
-#HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PATH = "E:\\Utilities\\Qt\\ProcessKilnImages\\data"
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 host = socket.gethostbyname(socket.gethostname())
 
+def recv_file(sock, file, n):
+    # Helper function to recv n bytes or return None if EOF is hit
+    total = 0
+    while True:
+        packet = sock.recv(min (4096,  n))
+        if not packet:
+            return
+        file.write (packet)
+        total += len(packet)
+        print ("read {0} bytes".format (total))
+        n -= len (packet)
+
+def recv_command (sock):
+    data = sock.recv (1)
+    return data.decode('utf-8')
+
+def recv_fileinfo (sock):
+    data = sock.recv (104)
+    (filesize, fn) = struct.unpack ('=I100s', data)
+    filename = fn.decode ('utf-8')
+    return (filename.strip(), filesize)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.bind((host, PORT))
-	s.listen()
-	while True:
-		conn, addr = s.accept()
-		with conn:
-			print('Connected by', addr)
-			while True:
-				data = conn.recv(1024)
-				if not data:
-					print ("closing connection")
-					break
-				str = data.decode('utf-8')
-				print ("Received: [{0}]".format(str))
-				if (str[0] == 'R'):
-					print ("read file and process")
-					conn.sendall(b'1702')
-				elif (str[0] == 'X'):
-					print ("ReadKilnTempImage exiting.")
-					conn.sendall(b'Shutting down')
-					exit ()
-				else:
-					print ("Unexpected command: {0}".format (str))
-					conn.sendall(b'Unexpected command: ' + data)
-					break
+    s.bind((host, PORT))
+    s.listen()
+    while True:
+        sock, addr = s.accept()
+        with sock:
+            print('Connected by', addr)
+            while True:
+                c = recv_command (sock)
+                if (c == 'X'):
+                    print ("ReadKilnTempImage exiting.")
+                    sock.sendall(b'Shutting down')
+                elif (c == 'F'):
+                    print ("Receiving a file...")
+                    (filename, filesize) = recv_fileinfo (sock)
+                    with open (os.path.join (PATH, filename), "wb") as file:
+                        recv_file (sock, file, filesize)
+                    print ("process file and return temperature")
+                    sock.sendall(b'1702')
+                elif (c == ''):
+                    print ("remote application closed socket")
+                    break
+                else:
+                    print ("Unexpected command: {0}".format (c))
+                    sock.sendall(b'Unexpected command: ' + c.encode ('utf-8'))
+                    break
 
--- TestSubmitKilnTemp.py
+++ TestSubmitKilnTemp.py
@@ -1,29 +1,53 @@
 import socket
 import os
+import struct
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+#HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
+PATH = "E:\\Utilities\Qt\\ProcessKilnImages\\data\\images"
 
 
-def SubmitFile (filename):
+def SubmitFile (orig_filename):
     host = socket.gethostbyname(socket.gethostname())
 
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((host, PORT))
+            c = 'F'
+            filename = os.path.join (PATH, orig_filename)
+            fs = int (os.path.getsize(filename))
+
+            # 1 byte command
+            # 4 bytes for file size
+            # 100 bytes for file name
+            # total 105 bytes to be sent
+            data = struct.pack ('=cI100s', c.encode('utf-8'), fs, "{:<100}".format (orig_filename).encode ('utf-8'))
+            s.send (data)
+            total = 0
             with open(filename, "rb") as f:
                 byte = f.read(4096)
                 while byte != b"":
-                    sa.send (byte)
+                    total += len(byte)
+                    s.send (byte)
+                    print ("Sent {0} bytes".format (total))
                     byte = f.read(4096)
                 data = s.recv(1024)
                 print('Response: [{0}]'.format (data.decode('utf-8')))
     except ConnectionError as e:
-        print ("connection error for host {0} {1} {2}".format(HOST, e.errno, e.strerror))
+        print ("connection error for host {0} {1} {2}".format(host, e.errno, e.strerror))
 
     except IOError as e:
         print ("transmission error on file {0} {1} {2}".format(filename, e.errno, e.strerror))
 
-filename = "./data/image/xxx.png"
-SubmitFile (filename)
+orig_filename = "K2019-09-23 06.34.14.png"
+
+# c = 'F'
+# fs = int (os.path.getsize(filename))
+# # data = struct.pack ('I', fs)
+# data = struct.pack ('c', c.encode('utf-8'))
+# data = struct.pack ('=cI', c.encode('utf-8'), fs)
+# # b = "{:<2}".format (filename).encode ('utf-8')
+# # data = struct.pack ('50s', "{:<50}".format (filename).encode ('utf-8'))
+# data = struct.pack ('=cI100s', c.encode('utf-8'), fs, "{:<100}".format (filename).encode ('utf-8'))
+SubmitFile (orig_filename)
 
